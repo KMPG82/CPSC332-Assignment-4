5c40a708f0be5b4242f0321ef776c61a25341165
Kevin <kevinponting82@gmail.com>
2024-03-17 22:42:24 -0700

    updated db

--- library_db.sql
+++ library_db.sql
@@ -5,44 +5,44 @@ USE Library;
 
 CREATE TABLE BORROWER(
 Card_no		char(11) not null,
-Name		varchar(50) not null,
-Address		varchar(100) not null,
+Name		varchar(30) not null,
+Address		varchar(30) not null,
 Phone		char(10) not null,
 CONSTRAINT pk_borrower primary key (Card_no)
 );
 
 CREATE TABLE LIBRARY_BRANCH(
 Branch_id		char(5) not null,
-Branch_name			varchar(50) not null,
-Address		varchar(100) not null,
+Branch_name		varchar(30) not null,
+Address		varchar(30) not null,
 CONSTRAINT pk_branch primary key (Branch_id)	
 );
 
 CREATE TABLE PUBLISHER (
-Name		varchar(100) not null,
-Address		varchar(100) not null,
+Name		varchar(30) not null,
+Address		varchar(30) not null,
 Phone		char(10) not null,
 CONSTRAINT pk_publisher primary key (Name)
 );
 
 CREATE TABLE BOOK (
-Book_id		char(9) not null,
-Publisher_name      varchar(100) not null,
-Title      varchar(100) not null, 
+Book_id		char(13) not null,
+Publisher_name		varchar(30) not null,
+Title		varchar(30) not null, 
 CONSTRAINT pk_book primary key (Book_id),
 CONSTRAINT fk_book_publisher foreign key (Publisher_name) references PUBLISHER(Name) 
 );
 
 CREATE TABLE BOOK_AUTHORS(
-Book_id		char(9) not null,
-Author_name		varchar(50),
+Book_id		char(13) not null,
+Author_name		varchar(30) not null,
 CONSTRAINT pk_authors primary key (Book_id, Author_name),
 CONSTRAINT fk_authors_book foreign key (Book_id) references BOOK(Book_id)
 );
 
 CREATE TABLE BOOK_COPIES(
-Book_id		char(9) not null,
-Branch_id		char(5),
+Book_id		char(13) not null,
+Branch_id		char(5) not null,
 No_of_copies		int not null,
 CONSTRAINT pk_copies primary key (Book_id, Branch_id),
 CONSTRAINT fk_copies_book foreign key (Book_id) references BOOK(Book_id),
@@ -50,7 +50,7 @@ CONSTRAINT fk_copies_branch foreign key (Branch_id) references LIBRARY_BRANCH(Br
 );
 
 CREATE TABLE BOOK_LOANS(
-Book_id		char(9) not null,
+Book_id		char(13) not null,
 Branch_id		char(5) not null,
 Card_no		char(11) not null,
 Date_out		date not null,
@@ -60,5 +60,3 @@ CONSTRAINT fk_loan_book foreign key (Book_id) references BOOK(Book_id),
 CONSTRAINT fk_loan_branch foreign key (Branch_id) references LIBRARY_BRANCH(Branch_id),
 CONSTRAINT fk_loan_borrower foreign key (Card_no) references BORROWER(Card_no)
 );
-
-
